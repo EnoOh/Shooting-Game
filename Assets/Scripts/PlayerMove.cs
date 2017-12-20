09ec741c0448475021f3b80195a1def91816a729
Enomoto <enoohji0817@gmail.com>
2017-12-21 02:07:15 +0900

    プロジェクトファイル追加

---
+++ Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour {
+    float direction = 0f;
+    public float speed = 10f;
+    Rigidbody2D rb2d;
+    Vector3 playerPos;
+
+	void Start () {
+        rb2d = GetComponent<Rigidbody2D>();
+	}
+	
+	void Update () {
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            direction = 1f;
+        }else if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            direction = -1f;
+        }
+        else
+        {
+            direction = 0f;
+        }
+        rb2d.velocity = new Vector2(speed * direction, rb2d.velocity.y);      
+	}
+}
