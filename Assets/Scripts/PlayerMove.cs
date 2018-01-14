36c5e889c3ea8c2da511fd864ec558c62ee75803
Enomoto <enoohji0817@gmail.com>
2018-01-14 18:25:47 +0900

    unity remote5の使用に伴い、タッチ操作に変更

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -2,29 +2,23 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMove : MonoBehaviour {
-    float direction = 0f;
-    public float speed = 10f;
-    Rigidbody2D rb2d;
-    Vector3 playerPos;
+public class PlayerMove : MonoBehaviour
+{
 
-	void Start () {
-        rb2d = GetComponent<Rigidbody2D>();
-	}
-	
-	void Update () {
-        //キャラ移動
-        if (Input.GetKey(KeyCode.RightArrow))
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+       if(Input.touchCount > 0)
         {
-            direction = 1f;
-        }else if (Input.GetKey(KeyCode.LeftArrow))
-        {
-            direction = -1f;
+            Touch touch = Input.GetTouch(0);
+            Vector3 touchPos = touch.position;
+            touchPos.z = 10.0f;
+            touchPos = Camera.main.ScreenToWorldPoint(touchPos);
+            transform.position = touchPos;
         }
-        else
-        {
-            direction = 0f;
-        }
-        rb2d.velocity = new Vector2(speed * direction, rb2d.velocity.y);      
-	}
+    }
 }
