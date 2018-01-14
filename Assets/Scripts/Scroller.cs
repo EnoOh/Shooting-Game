d548b6581736c19a1dbc47d7cae70b4cf36b1e20
Enomoto <enoohji0817@gmail.com>
2018-01-14 19:54:48 +0900

    スクロール機能の作り直し

---
+++ Assets/Scripts/Scroller.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scroller : MonoBehaviour {
+
+	void Start () {
+		
+	}
+
+    private void FixedUpdate()
+    {
+        transform.Translate(0, -0.1f, 0);
+        if (transform.position.y < -10.6f)
+        {
+            transform.position = new Vector3(0, 10.6f, 0);
+        }
+    }
+
+    void Update () {
+        
+	}
+}
