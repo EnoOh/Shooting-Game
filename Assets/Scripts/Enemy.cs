c269458ec2d006baf9b3134e9c72c8613301728c
Enomoto <enoohji0817@gmail.com>
2018-02-25 19:14:52 +0900

    色々更新

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+    //敵の速度
+    public float enemySpeed;
+    //敵が弾を撃つ間隔
+    public float shotDelay;
+    //敵の弾
+    public GameObject enemyBullet;
+
+	void Start () {
+        
+	}
+	
+	void Update () {
+		
+	}
+
+    public void Shot()
+    {
+
+    }
+
+    public void Move()
+    {
+
+    }
+}
